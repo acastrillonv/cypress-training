025c7e93ecee4dabcba767d30a8fa3a157d6256d
acastrillonv <acastrillonv@gmail.com>
2022-04-16 22:57:34 -0500

    buy shirt test

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -11,12 +11,17 @@
 
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
+/// <reference types="cypress" />
+
+const wp = require('@cypress/webpack-preprocessor')
 
 /**
  * @type {Cypress.PluginConfig}
  */
-// eslint-disable-next-line no-unused-vars
 module.exports = (on, config) => {
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
+  const options = {
+    webpackOptions: require('../../webpack.config'),
+  }
+
+  on('file:preprocessor', wp(options))
 }
