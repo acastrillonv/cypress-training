9c85984fa17c3d5eb7960fdf162107cd3f9f154f
acastrillonv <acastrillonv@gmail.com>
2022-04-19 21:49:27 -0500

    AAA exercise completed, added dresses-list.page and dresses-list.spec

--- cypress/page/menu-content.page.ts
+++ cypress/page/menu-content.page.ts
@@ -1,10 +1,12 @@
 class MenuContentPage {
     private tShirtMenu: string;
+    private dressesMenu:string;
     private menuContentPageURL: string
 
     constructor() {
         this.tShirtMenu = '#block_top_menu > ul > li:nth-child(3) > a';
-        this.menuContentPageURL = 'http://automationpractice.com/'
+        this.menuContentPageURL = 'http://automationpractice.com/';
+        this.dressesMenu='#block_top_menu > ul > li:nth-child(2) > a';
     }
 
     public visitMenuContentPage(): void {
@@ -13,7 +15,10 @@ class MenuContentPage {
 
     public goToTShirtMenu(): void {
         cy.get(this.tShirtMenu).click()
-    }    
+    }
+    public goToDressesMenu(): void {
+        cy.get(this.dressesMenu).click()
+    }     
 
 }
 export { MenuContentPage }
